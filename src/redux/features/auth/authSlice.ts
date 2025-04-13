fee7feb2face7bf5dafadcec1887dd175e0158f0
al-amin90 <ijesun30@gmail.com>
2025-04-13 13:15:10 +0600

    authslice added & baseapi

---
+++ src/redux/features/auth/authSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type TInitialState = {
+  user: string | null;
+  token: string | null;
+};
+
+const initialState: TInitialState = {
+  user: null,
+  token: null,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      const { user, token } = action.payload;
+      state.user = user;
+      state.token = token;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
