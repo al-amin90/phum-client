b55755d90ce711469c1a1d0d59b0e5c252e5bb53
al-amin90 <ijesun30@gmail.com>
2025-04-14 13:19:04 +0600

    token added

--- src/redux/api/baseAPi.ts
+++ src/redux/api/baseAPi.ts
@@ -4,20 +4,24 @@ import type { RootState } from "../store";
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
   credentials: "include",
-  prepareHeaders: (headers, {getState}) => {
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
 
-    const token = (getState() as RootState).auth.token
-
-    if(token){
-      headers.set('authorization', `${token}`)
+    if (token) {
+      headers.set("authorization", `${token}`);
     }
 
-    return headers
-  }
-})
+    return headers;
+  },
+});
+
+const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+  console.log(result);
+};
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
